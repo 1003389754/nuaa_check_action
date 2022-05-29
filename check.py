6a2673cc3e0f2de245249d57ccec0c4a37de8747
Mrchenuo <mynuo@outlook.com>
2022-05-29 20:17:11 +0800

    custom

--- check.py
+++ check.py
@@ -7,10 +7,11 @@ import time
 import traceback
 import requests
 from send_mail import send_mail
+
 # from requests_toolbelt.utils import dump
 
-try_times = 1   # 失败这么多次后就直接不管了
-delay = 2   # 访问页面前的延迟，为了防止过快访问网站被封IP
+try_times = 1  # 失败这么多次后就直接不管了
+delay = 2  # 访问页面前的延迟，为了防止过快访问网站被封IP
 
 
 # 登陆并且返回json形式的cookie，如果登陆失败返回空串
@@ -21,7 +22,7 @@ def login(stu_number, password):
         try:
             time.sleep(delay)
             response = requests.get(
-                'https://m.nuaa.edu.cn/uc/wap/login', cookies=cookies)
+                'http://m.nuaa.edu.cn/uc/wap/login', cookies=cookies)
             print('get login page:', response.status_code)
 
             # cookies = response.headers['Set-Cookie']
@@ -29,7 +30,7 @@ def login(stu_number, password):
             cookies = dict(response.cookies)
 
             time.sleep(delay)
-            response = requests.get('https://m.nuaa.edu.cn/uc/wap/login/check', cookies=cookies,
+            response = requests.get('http://m.nuaa.edu.cn/uc/wap/login/check', cookies=cookies,
                                     data='username={}&password={}'.format(stu_number, password))
             print('login...:', response.status_code)
 
@@ -37,7 +38,7 @@ def login(stu_number, password):
             # cookies = cookies + '; ' + \
             #     re.search(r'UUkey=([a-zA-Z0-9]+)', cookies2).group(0)
             cookies.update(dict(response.cookies))
-            
+
             # print(cookies)
             print(response.text)
             return cookies, '登陆结果：' + response.text + '\n'
@@ -48,6 +49,7 @@ def login(stu_number, password):
     # raise Exception('lOGIN FAIL')
     return {}, '登陆结果：login faild,请检查账号密码\n'
 
+
 # longitude: 经度； latitude: 纬度
 # 根据经纬度访问高德API，并且返回打卡时候“geo_api_info”字段的值
 def get_address_info(longitude, latitude):
@@ -55,7 +57,7 @@ def get_address_info(longitude, latitude):
         try:
             time.sleep(delay)
             response = requests.get(
-                'https://restapi.amap.com/v3/geocode/regeo', params={
+                'http://restapi.amap.com/v3/geocode/regeo', params={
                     'key': '729923f88542d91590470f613adb27b5',
                     's': 'rsv3',
                     'location': str(longitude) + ',' + str(latitude)
@@ -72,8 +74,8 @@ def get_address_info(longitude, latitude):
                 },
                 "location_type": "html5",
                 "message": "Get ipLocation failed.Get geolocation success.Convert Success.Get address success.",
-                "accuracy": 102,    # ???
-                "isConverted": True,    # ?
+                "accuracy": 102,  # ???
+                "isConverted": True,  # ?
                 "status": 1,
                 "addressComponent": {
                     "citycode": geo_data['addressComponent']['citycode'],
@@ -103,197 +105,136 @@ def get_address_info(longitude, latitude):
     return geo_api_info
     # print(dump.dump_all(response).decode('utf-8'))
 
+
 # 获取uid，id，打卡时候会用到，获取失败异常最可能的原因是账号密码错误
 def get_uid_id(cookies):
     for _ in range(try_times):
         try:
             time.sleep(delay)
             response = requests.get(
-                'https://m.nuaa.edu.cn/ncov/wap/default', cookies=cookies)
+                'http://m.nuaa.edu.cn/ncov/wap/default', cookies=cookies)
             response.encoding = 'utf-8'
             uid = re.search(r'"uid":"([0-9]*)"', response.text).group(1)
             id = re.search(r'"id":([0-9]*)', response.text).group(1)
-            return uid,id, 'UID获取成功\n'
+            return uid, id, 'UID获取成功\n'
         except:
             traceback.print_exc()
     # 就这样吧，让他崩溃，万一假打卡了就不好了
     print('获取id、uid失败')
     return False, '获取id、uid失败\n'
 
+
 # 签到，返回True成功，否则失败
 def check(cookies, geo_api_info, id, uid):
     # Post的data，如果你是勇士可以尝试给这个打上注释，老谜语人了，看不懂ヾ(•ω•`)o
     data = {
-        # 'sfzhux': '1',
-        # 'zhuxdz': '',# 住校地址
-        # 'szgj': '',
-        # 'szcs': '',
-        # 'szgjcs': '',
-        # 'sfjwfh': '0',
-        # 'sfyjsjwfh': '0',
-        # 'sfjcjwfh': '0',
-        # 'sflznjcjwfh': '0',
-        # 'sflqjkm': '4',
-        # 'jkmys': '1',
-        # 'sfjtgfxdq': '0',
-        # 'nuaaxgymjzqk': '1',
-        # 'dyzjzsj': '',
-        # 'bfhzjyq': '0',
-        # 'hxyxjzap': '0',
-        # 'yjzjsj': '',
-        # 'sftjlkjc': '',
-        # 'lkjctlsj': '',
-        # 'sfsylkjcss': '',
-        # 'gzczxq': '1',
-        # 'zrwjtw': '1',
-        # 'jrzjtw': '1',
-        # 'ifcyglq': '0',
-        # 'wskmyy': '',
-        # 'zhycjgdqifjn': '',
-        # 'dqsfzgfxszqs': '0',
-        # 'gqsfyzgfxljs': '0',
-        # 'gqsfyqzfhryjc': '0',
-        # 'sfyjwljqyhg': '0',
-        # 'cjfxsfhs': '1',
-        # 'bzxyy': '',
-        # 'bzxyydesc': '',
-        # 'id': id,# 打卡的ID，其实这个没影响的
-        # 'uid': uid,  # UID
-        # 'date': time.strftime("%Y%m%d", time.localtime()),  # 打卡年月日一共8位
-        # 'tw': '2',
-        # 'sfcxtz': '0',
-        # 'sfyyjc': '0',
-        # 'jcjgqr': '0',
-        # 'jcjg': '',
-        # 'sfjcbh': '0',
-        # 'sfcxzysx': '0',
-        # 'qksm': '',
-        # 'remark': '',
-        # 'address': geo_api_info['formattedAddress'],
-        # 'area': geo_api_info['addressComponent']['province'] + ' ' + geo_api_info['addressComponent']['city']+ ' ' + geo_api_info['addressComponent']['district'],
-        # 'province': geo_api_info['addressComponent']['province'],
-        # 'city': geo_api_info['addressComponent']['city'],
-        # 'geo_api_info': json.dumps(geo_api_info, separators=(',', ':')),
-        # 'created': round(time.time()), # 时间戳
-        # 'sfzx': '1',
-        # 'sfjcwhry': '0',
-        # 'sfcyglq': '0',
-        # 'gllx': '',
-        # 'glksrq': '',
-        # 'jcbhlx': '',
-        # 'jcbhrq': '',
-        # 'sftjwh': '0',
-        # 'sftjhb': '0',
-        # 'fxyy': '',
-        # 'bztcyy': '',
-        # 'fjsj': '0',
-        # 'created_uid': '0',
-        # 'sfjchbry': '0',
-        # 'sfjcqz': '',
-        # 'jcqzrq': '',
-        # 'jcwhryfs': '',
-        # 'jchbryfs': '',
-        # 'xjzd': '',
-        # 'sfsfbh': '0',
-        # 'sfjcwzry': '0',
-        # 'sftjwz': '0',
-        # 'jhfjrq': '',
-        # 'jhfjjtgj': '',
-        # 'jhfjhbcc': '',
-        # 'jhfjsftjwh': '0',
-        # 'jhfjsftjhb': '0',
-        # 'szsqsfybl': '0',
-        # 'sfygtjzzfj': '0',
-        # 'gtjzzfjsj': '',
-        # 'sfsqhzjkk': '0',
-        # 'sqhzjkkys': '',
-        # 'is_fx_log': '0',
-        # 'fxzrwjtw': '0',
-        # 'fxjrcjtw': '0',
-        # 'fxjrzjtw': '0',
-        # 'fxsfzx': '0',
-        # 'fxsfcyglq': '0',
-        # 'fxsfcxtz': '0',
-        # 'sfjzxg': '0',
-        # 'skmcolor': '1',
-        # 'skmimg': '',
-        # 'ifjrglkjc': '',
-        # 'gtjjsfhm': '',
-        # 'gtjzsfhzl': '',
-        # 'sffhddjjgc': '',
-        # 'ifjgzgfxq': '',
-        # 'jgzgfxrq': '',
-        # 'jgzgfxdq': '',
-        # 'jgzgfxxxdz': '',
-        # 'newwchsjccs': '',
-        # 'dsdecjcsj': '',
-        # 'dsdechsjcjgtype': '',
-        # 'dsdrchsjcdd': '',
-        # 'dsdechsjcjg': '',
-        # 'zhyccjcsj': '',
-        # 'zhycchsjcjgtype': '',
-        # 'zhycchsjcdd': '',
-        # 'zhycchsjcjg': '',
-        # 'gwszdd': '',
-        # 'sfyqjzgc': '',
-        # 'jrsfqzys': '',
-        # 'jrsfqzfy': '',
-        # 'ismoved': '0',
-
-        'sfzhux': '0', # 是否住校
+        'sfzhux': '1',
+        'zhuxdz': '',
+        'szgj': '',
+        'szcs': '',
+        'szgjcs': '',
         'sfjwfh': '0',
         'sfyjsjwfh': '0',
         'sfjcjwfh': '0',
         'sflznjcjwfh': '0',
         'sflqjkm': '4',
-        'jkmys': '1',
-        'sfjtgfxdq': '0',  # 是否经停高风险地区
+        'jkmys': '0',
+        'sfjtgfxdq': '0',
         'nuaaxgymjzqk': '1',
+        'dyzjzsj': '',
         'bfhzjyq': '0',
         'hxyxjzap': '0',
-        'sfmrhs': '1',
+        'yjzjsj': '',
         'sfjkyc': '0',
-        'gzczxq': '1',
-        'zrwjtw': '1',  # 昨日晚间体温 1: (37-) 2:(37-37.2) 3:(37.3-37.5) 4:(37.5+) 3,4会无法入校
-        'jrzjtw': '1',  # 今日早间体温 1: (37-) 2:(37-37.2) 3:(37.3-37.5) 4:(37.5+)
+        'sftjlkjc': '',
+        'lkjctlsj': '',
+        'sfsylkjcss': '',
+        'gjzsftjlkjc': '',
+        'gjzlkjctlsj': '',
+        'gjzsfsylkjcss': '',
+        'ifhxjc': '',
+        'hsjconetime': '',
+        'hsjconeplace': '',
+        'hsjconejg': '',
+        'hsjctwotime': '',
+        'hsjctwoplace': '',
+        'hsjctwojg': '',
+        'hsjcthreetime': '',
+        'hsjcthreeplace': '',
+        'hsjcthreejg': '',
+        'hsjcfourtime': '',
+        'hsjcfourplace': '',
+        'hsjcfourjg': '',
+        'ywchxjctime': '',
+        'hsjclist': '{}',
+        'njrddz': '南京航空航天大学将军路校区南区',
+        'gzczxq': '2',
+        'ifznqgfxljs': '',
+        'iflsqgfxljs': '',
+        'zrwjtw': '1',
+        'jrzjtw': '1',
+        'jrlvymjrq': '',
         'ifcyglq': '0',
+        'wskmyy': '',
+        'zhycjgdqifjn': '',
         'dqsfzgfxszqs': '0',
         'gqsfyzgfxljs': '0',
         'gqsfyqzfhryjc': '0',
         'sfyjwljqyhg': '0',
-        'cjfxsfhs': '1',
-        'bzxyy': '1',
-        'id': id,
-        'uid': uid,
+        'cjfxsfhs': '',
+        'bzxyy': '',
+        'bzxyydesc': '',
+        'id': id,  # 打卡的ID，其实这个没影响的
+        'uid': uid,  # UID
         'date': time.strftime("%Y%m%d", time.localtime()),  # 打卡年月日一共8位
         'tw': '2',
         'sfcxtz': '0',
         'sfyyjc': '0',
         'jcjgqr': '0',
+        'jcjg': '',
         'sfjcbh': '0',
         'sfcxzysx': '0',
+        'qksm': '',
+        'remark': '',
         'address': geo_api_info['formattedAddress'],
-        'area': geo_api_info['addressComponent']['province'] + ' ' + geo_api_info['addressComponent']['city']+ ' ' + geo_api_info['addressComponent']['district'],
+        'area': geo_api_info['addressComponent']['province'] + ' ' + geo_api_info['addressComponent']['city'] + ' ' +
+                geo_api_info['addressComponent']['district'],
         'province': geo_api_info['addressComponent']['province'],
         'city': geo_api_info['addressComponent']['city'],
         'geo_api_info': json.dumps(geo_api_info, separators=(',', ':')),
-        'created': round(time.time()), # 时间戳
-        'sfzx': '0',
+        'created': round(time.time()),  # 时间戳
+        'sfzx': '1',
         'sfjcwhry': '0',
         'sfcyglq': '0',
+        'gllx': '',
+        'glksrq': '',
+        'jcbhlx': '',
+        'jcbhrq': '',
         'sftjwh': '0',
         'sftjhb': '0',
-        'bztcyy': '4',
+        'fxyy': '',
+        'bztcyy': '',
         'fjsj': '0',
+        'created_uid': '0',
         'sfjchbry': '0',
+        'sfjcqz': '',
+        'jcqzrq': '',
+        'jcwhryfs': '',
+        'jchbryfs': '',
+        'xjzd': '',
         'sfsfbh': '0',
         'sfjcwzry': '0',
         'sftjwz': '0',
+        'jhfjrq': '',
+        'jhfjjtgj': '',
+        'jhfjhbcc': '',
         'jhfjsftjwh': '0',
         'jhfjsftjhb': '0',
         'szsqsfybl': '0',
         'sfygtjzzfj': '0',
+        'gtjzzfjsj': '',
         'sfsqhzjkk': '0',
+        'sqhzjkkys': '',
         'is_fx_log': '0',
         'fxzrwjtw': '0',
         'fxjrcjtw': '0',
@@ -303,14 +244,40 @@ def check(cookies, geo_api_info, id, uid):
         'fxsfcxtz': '0',
         'sfjzxg': '0',
         'skmcolor': '1',
+        'skmimg': '',
+        'ifjrglkjc': '',
+        'gtjjsfhm': '',
+        'gtjzsfhzl': '',
+        'sffhddjjgc': '',
+        'ifjgzgfxq': '',
+        'jgzgfxrq': '',
+        'jgzgfxdq': '',
+        'jgzgfxxxdz': '',
+        'newwchsjccs': '',
+        'dsdecjcsj': '',
+        'dsdechsjcjgtype': '',
+        'dsdrchsjcdd': '',
+        'dsdechsjcjg': '',
+        'zhyccjcsj': '',
+        'zhycchsjcjgtype': '',
+        'zhycchsjcdd': '',
+        'zhycchsjcjg': '',
+        'hsjczm': '',
+        'sfmrhs': '',
+        'gtjzryxcsfyc': '1',
+        'gtjzryxcsfycQt': '',
+        'gwszdd': '',
+        'sfyqjzgc': '',
+        'jrsfqzys': '',
+        'jrsfqzfy': '',
         'ismoved': '0'
     }
     for _ in range(try_times):
         try:
             time.sleep(delay)
-            response = requests.post('https://m.nuaa.edu.cn/ncov/wap/default/save', data=data, cookies=cookies)
+            response = requests.post('http://m.nuaa.edu.cn/ncov/wap/default/save', data=data, cookies=cookies)
             print('sign statue code:', response.status_code)
-            #print('sign return:', response.text) 
+            # print('sign return:', response.text)
             response.encoding = 'utf-8'
 
             if response.text.find('成功') >= 0:
@@ -334,9 +301,13 @@ def send_result(config, recever, result, messgae):
         send_mail(mail_sender, smtp_password, smtp_host,
                   recever, messgae, '打卡失败', '主人', '打卡姬')
 
+
 def main():
     config = sys.stdin.read()
     config = json.loads(config)
+    # config = []
+    with open("stu.json", 'r', encoding='UTF-8') as f:
+        config = json.load(f)
 
     for student in config['students']:
         result = False  # 打卡结果，False表示没有打上
