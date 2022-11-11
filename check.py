34371238b7a8e8dbd6320e2c58be01ce6c08fa30
Timecollector <43028338+Timecollector@users.noreply.github.com>
2022-11-11 16:12:12 +0800

    Update check.py

--- check.py
+++ check.py
@@ -124,49 +124,82 @@ def get_uid_id(cookies):
 def check(cookies, geo_api_info, id, uid):
     # Post的data，如果你是勇士可以尝试给这个打上注释，老谜语人了，看不懂ヾ(•ω•`)o
     data = {
-        'sfzhux': '0',
-        'zhuxdz': '',
+        # 是否住校 是：1
+        'sfzhux': '1',
+        # 住校地址
+        'zhuxdz': '101',
+        # 所在地点
         'szgj': '',
+        # 所在城市
         'szcs': '',
+        # 所在国家城市
         'szgjcs': '',
+        # 今日是否在中高风险地区？否：0
         'sfjwfh': '0',
+        # 今日是否从中高风险地区（含家属含境外）返回？ 否：0
         'sfyjsjwfh': '0',
+        # 不知道
         'sfjcjwfh': '0',
+        # 今日是否与从中高风险地区（含境外）返回的人员有过密切接触（不含已经解除隔离的境外返回人员）？ 否：0
         'sflznjcjwfh': '0',
+        # 是否拥有健康码 是：4
         'sflqjkm': '4',
+        # 苏康码颜色 绿色：1
         'jkmys': '1',
+        # 大概是健康码非绿触发的选项
         'sfjtgfxdq': '0',
-        'tw': '2',
+        # 体温 选1
+        'tw': '1',
+        # 今日是否出现发热、干咳、咽痛、流涕、腹泻、乏力、嗅（味）觉减退、肌肉酸痛等症状？ 否：0
         'sfcxtz': '0',
+        # 今日是否接触疑似/确诊人群？ 否：0
         'sfjcbh': '0',
+        # 大概是接触疑似衍生选项
         'sfcxzysx': '0',
+        # 不知道
         'qksm': '',
+        # 是否到相关医院或门诊检查？
         'sfyyjc': '0',
+        # 检查结果属于以下哪种情况
         'jcjgqr': '0',
         'remark': '',
+        # 地址
         'address': geo_api_info['formattedAddress'],
         'geo_api_info': json.dumps(geo_api_info, separators=(',', ':')),
         'area': geo_api_info['addressComponent']['province'] + ' ' + geo_api_info['addressComponent']['city']
                  + ' ' + geo_api_info['addressComponent']['district'],
         'province': geo_api_info['addressComponent']['province'],
         'city': geo_api_info['addressComponent']['city'],
-        'sfzx': '0',
+        # 是否在校 是：1
+        'sfzx': '1',
         'sfjcwhry': '0',
         'sfjchbry': '0',
         'sfcyglq': '0',
+        # 隔离/医学观察场所
         'gllx': '',
+        # 隔离/医学观察开始时间
         'glksrq': '',
+        # 接触人群类型
         'jcbhlx': '',
+        # 接触日期
         'jcbhrq': '',
+        # 当前地点与上次不在同一城市，原因如下
+        # 1其他，2探亲，3旅游，4回家，5返宁，6出差
         'bztcyy': '',
+        # 不知道
         'sftjhb': '0',
+        # 不知道
         'sftjwh': '0',
+        # 不知道
         'sftjwz': '0',
+        # 不知道
         'sfjcwzry': '0',
+        # 检查结果
         'jcjg': '',
         'date': time.strftime("%Y%m%d", time.localtime()),  # 打卡年月日一共8位
         'uid': uid,  # UID
         'created': round(time.time()), # 时间戳
+        # 不知道
         'jcqzrq': '',
         'sfjcqz': '',
         'szsqsfybl': '0',
@@ -180,6 +213,7 @@ def check(cookies, geo_api_info, id, uid):
         'sfyqjzgc': '',
         'jrsfqzys': '',
         'jrsfqzfy': '',
+        # 判断坐标是否相同，相同为0
         'ismoved': '0'
     }
     for _ in range(try_times):
